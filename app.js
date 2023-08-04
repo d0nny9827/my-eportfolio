0f501d930319d7981cd58646c64dc2dbbe933376
Donovan <donovanlopez9827@gmail.com>
2023-08-04 00:52:01 -0700

    emailjs feature added

---
+++ app.js
@@ -0,0 +1,25 @@
+function contact(event) {
+  event.preventDefault();
+
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_iup23dv",
+      "template_3mnch2k",
+      event.target,
+      "xfiG7g_lLE9lP101Y"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      alert(
+        "The email service is temporarily down, please contact me directly at donovanlopez9827@gmail.com"
+      );
+    });
+}
